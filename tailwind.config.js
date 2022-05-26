15ef1a7ba02800a669187f80edf7a00655be6406
NasirUddinNahid <nahidahmed17762@gmail.com>
2022-05-27 01:01:08 +0600

    firebase

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
-// module.exports = {
-//   content: ["./src/**/*.{html,js}"],
-//   theme: {
-//     extend: {},
-//   },
-//   daisyui: {
-//     themes: [
-//       {
-//         mytheme: {
-//           primary: "#5442c9",
-//           secondary: "#19D3AE",
-//           accent: "#3A4256",
-//           neutral: "#3d4451",
-//           "base-100": "#ffffff",
-//           "btn-color":"#42c9ac",
-//         },
-//       },
-//       "dark",
-//       "cupcake",
-//     ],
-//   },
-//   plugins: [require("daisyui")],
-// }
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#5442c9",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+          "btn-color":"#42c9ac",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
